18274aa04a4e07058233f071177f89f4059e68c9
gbursson <gbursson@gmail.com>
2019-03-06 02:23:45 +0000

    moved all source files to _src folder. refactored gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const postCSS = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const del = require("del");
-const cssNano = require("cssnano");
+// const cssNano = require("cssnano");
 const importCSS = require("postcss-import");
 const rename = require("gulp-rename");
 const browserSync = require("browser-sync");
@@ -15,17 +15,13 @@ const postCssPlugins = [
     browsers: ["> 1%"],
     cascade: false
   })
-  // unCss({
-  //   html: ['index.html']
-  // }),
-  //
   // uncomment cssNano for production
   // cssNano()
 ];
 
 const cleanup = (cb) => del("_out/");
-const html = (cb) => gulp.src("index.html").pipe(gulp.dest("_out")).pipe(browserSync.stream());
-const css = (cb) => gulp.src("css/styles.css")
+const html = (cb) => gulp.src("_src/index.html").pipe(gulp.dest("_out")).pipe(browserSync.stream());
+const css = (cb) => gulp.src("_src/css/styles.css")
   .pipe(postCSS(postCssPlugins))
   .pipe(rename("style.min.css"))
   .pipe(gulp.dest("_out/"))
@@ -36,8 +32,8 @@ const serve = (cb) => browserSync.init({
   server: "_out"
 });
 
-const watchCSS = (cb) => gulp.watch("css/*.css", css);
-const watchHTML = (cb) => gulp.watch("index.html", html);
+const watchCSS = (cb) => gulp.watch("_src/css/*.css", css);
+const watchHTML = (cb) => gulp.watch("_src/index.html", html);
 
 
 exports.build = gulp.series(cleanup, gulp.parallel(css, html), gulp.parallel(watchCSS, watchHTML, serve));
